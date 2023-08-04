72f5d56735cc6a32a3b9d2e54e53102790859e68
Niklas Dahl <niklas.dahl@ray.fi>
2023-08-04 18:13:02 +0300

    split primary reactive tasks into subfunctions

--- index.js
+++ index.js
@@ -29,54 +29,66 @@ client.on('messageCreate', async (message) => {
     `*st` : a user with the appropriate role may enable/disable the active-ST role and tag "(ST)"'
     });
   } else if (message.content === '*!') {
-    if (!isSpecTagged) {
-      if (isStTagged) {
-        message.member.roles.remove(activeStorytellerRole).catch(console.error);
-      }
-      message.member.setNickname('!' + nonPrefixedDisplayName)
-        .catch((err) => replyUnableToChangeNick(message, '!' + nonPrefixedDisplayName, err));
-    } else {
-      message.member.setNickname(nonPrefixedDisplayName)
-        .catch((err) => replyUnableToChangeNick(message, nonPrefixedDisplayName, err));
-    }
+    specToggle(nonPrefixedDisplayName, isSpecTagged, isStTagged, message, activeStorytellerRole);
   } else if (message.content === '*st') {
-    if (!message.member.roles.cache.has(activeStorytellerRole.id)) {
-      if (message.member.roles.cache.has(storytellerRole.id)) {
-        message.member.roles.add(activeStorytellerRole).catch(console.error);
-        if (!isStTagged) {
-          message.member.setNickname('(ST) ' + nonPrefixedDisplayName)
-            .catch((err) => replyUnableToChangeNick(message, '(ST) ' + nonPrefixedDisplayName, err));
-        }
-      } else {
-        message.channel.send({content: '`you lack permission to claim the active storyteller role`'});
-      }
-    } else {
-      message.member.roles.remove(activeStorytellerRole).catch(console.error);
-      if (isStTagged) {
-        message.member.setNickname(nonPrefixedDisplayName)
-          .catch((err) => replyUnableToChangeNick(message, nonPrefixedDisplayName, err));
-      }
-    }
+    stToggle(nonPrefixedDisplayName, isStTagged, message, storytellerRole, activeStorytellerRole);
+  } else if (message.content.startsWith('!role ')) {
+    wikiCharacterLinkRequest(message);
   } else if (1==2) {
     let resp = await axios.get(`https://api.jsonapi.io/random`);
     const outMsg = resp.data.content;
     message.reply({content:outMsg});
-  } else if (message.content.startsWith('!role ')) {
-    const wikiUrl = 'https://wiki.bloodontheclocktower.com/'
-    const characterRoleName = message.content.substring(6);
-    // ALSO CHECK IF CHAR ROLE NAME HAS "-", and re-join appropriately..
-    const formattedCharacterRoleName = capitalizeRoleNameWithUnderscores(characterRoleName);
-//    console.log(formattedCharacterRoleName);
-    let resp = await axios
-            .head(wikiUrl + formattedCharacterRoleName)
-//            .catch (console.error());
-    if (resp.status && resp.status === 200) {
-      message.channel.send({content: wikiUrl + formattedCharacterRoleName});
-    } else {
-      message.channel({content: '`character role not found`'});
-    }
   }
 })
+
+function specToggle(nonPrefixedDisplayName, isSpecTagged, isStTagged, message, activeStorytellerRole) {
+  if (!isSpecTagged) {
+    if (isStTagged) {
+      message.member.roles.remove(activeStorytellerRole).catch(console.error);
+    }
+    message.member.setNickname('!' + nonPrefixedDisplayName)
+      .catch((err) => replyUnableToChangeNick(message, '!' + nonPrefixedDisplayName, err));
+  } else {
+    message.member.setNickname(nonPrefixedDisplayName)
+      .catch((err) => replyUnableToChangeNick(message, nonPrefixedDisplayName, err));
+  }
+}
+
+function stToggle(nonPrefixedDisplayName, isStTagged, message, storytellerRole, activeStorytellerRole) {
+  if (!message.member.roles.cache.has(activeStorytellerRole.id)) {
+    if (message.member.roles.cache.has(storytellerRole.id)) {
+      message.member.roles.add(activeStorytellerRole).catch(console.error);
+      if (!isStTagged) {
+        message.member.setNickname('(ST) ' + nonPrefixedDisplayName)
+          .catch((err) => replyUnableToChangeNick(message, '(ST) ' + nonPrefixedDisplayName, err));
+      }
+    } else {
+      message.channel.send({content: '`you lack permission to claim the active storyteller role`'});
+    }
+  } else {
+    message.member.roles.remove(activeStorytellerRole).catch(console.error);
+    if (isStTagged) {
+      message.member.setNickname(nonPrefixedDisplayName)
+        .catch((err) => replyUnableToChangeNick(message, nonPrefixedDisplayName, err));
+    }
+  }
+}
+
+async function wikiCharacterLinkRequest(message) {
+  const wikiUrl = 'https://wiki.bloodontheclocktower.com/'
+  const characterRoleName = message.content.substring(6);
+  // ALSO CHECK IF CHAR ROLE NAME HAS "-", and re-join appropriately..
+  const formattedCharacterRoleName = capitalizeRoleNameWithUnderscores(characterRoleName);
+//    console.log(formattedCharacterRoleName);
+  let resp = await axios
+          .head(wikiUrl + formattedCharacterRoleName)
+//            .catch (console.error());
+  if (resp.status && resp.status === 200) {
+    message.channel.send({content: wikiUrl + formattedCharacterRoleName});
+  } else {
+    message.channel({content: '`character role not found`'});
+  }
+}
 
 function getNonPrefixedName(displayName) {
   let formattedName = displayName;
